c083da2ff6dbc36977361c74b78fa8b6b6aafea4
diogo678 <diohosousa186@gmail.com>
2023-05-29 09:22:02 -0300

    primeiro commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.main')
+@section('title', 'produtos')  
+
+@section('content')
+
+
+<h2> teste de routa para volta para pagina home</h2>
+
+
+<a href="/produtos"> volta para home </a>
+@endsection
---
+++ resources/views/products.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+@section('title', 'produtos')  
+
+@section('content')
+
+
+    <h1> Teste de Routes </h1>
+
+
+
+@endsection
